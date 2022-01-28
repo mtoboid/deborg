58883cc8d7a3847c26dc89cec73d5691707a642c
Tobias Marczewski (mtoboid) <vortex@e.mail.de>
2022-01-28 18:14:02 +0100

    Improved extract_deb_package_from_line().

--- src/deborg/parser.py
+++ src/deborg/parser.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+
+from collections.abc import Sequence
 from dataclasses import dataclass
 import re
 
@@ -13,43 +15,85 @@ class DebPakInfo:
 
 class Parser:
     """Class that contains methods to parse an emacs .org file """
+    # symbols that can indicate a line in a list, which can contain a .deb package
     LIST_BULLETS: str = "-+"
-    # <package-name> {<distro-name>:<release>}
-    PACKAGE_AND_INFO_RX: re.Pattern = re.compile(
-        "\\s*(?P<package_name>[a-z0-9][-a-z0-9.+]+)" +
-        "(\\s+[{]\\s*" +
-        "(?P<distro_name>[\\w]+)" +
-        "(\\s*:\\s*(?P<release>[\\w.]+))?" +
-        "\\s*[}])?"
-    )
 
     @staticmethod
-    def _extract_deb_packages_from_line(line: str) -> list[DebPakInfo]:
+    def extract_deb_packages(file: str, distro: str, release: str) -> list[DebPakInfo]:
+        _lines: list[str]
+        _packages: list[DebPakInfo] = list()
+        with open(file, "r") as _file:
+            _lines = _file.readlines()
+        for _line in _lines:
+            _packages.append(Parser.extract_deb_package_from_line(_line, distro, release))
+        return _packages
+
+    @staticmethod
+    def extract_deb_package_from_line(line: str, distro: str, release: str) -> DebPakInfo | None:
         """
-        Parse a line of the form:
+        Parse a line containing deb package information, and return the deb package matching the specifications.
+
+        Parse a string of the form:
         <list-bullet> <package-name1> {<distro-name1>:<release1>}, <package-name2> {...}, ... :: <comment>
         where:
             <list-bullet> is either '+' or '-'
-            <package-name> - a valid Debian package name
-                (see https://www.debian.org/doc/manuals/debian-reference/ch02.en.html#_debian_package_file_names)
-            <distro-name> and <release> are strings containing any char apart from a ':' (colon)
+            <package-name> - a string without whitespaces
+            <distro-name> and <release> are strings containing word characters
         Only <list-bullet> and <package-name> are required, the other elements - content in {...} and ':: <comment>' -
         are optional.
 
-        :param line: The line to parse
-        :return: a list of DebPakInfo objects, or an empty list, if the line does not specify any deb packages.
+        :param line: The string to parse
+        :param distro: return package for which distro?
+        :param release: return package for which release?
+        :return: a DebPakInfo object, or None if none matches the specifications
         """
-        out: list[DebPakInfo] = list()
-        if Parser._is_package_line(line):
-            # remove <list-bullet>
-            _line: str = line.strip()[1:].strip()
-            # remove :: <comment>
-            _line = _line.split("::")[0].strip()
-            # parse packages and info
-            packages = _line.split(",")
-            for pak in packages:
-                out.append(Parser._get_package_info(pak))
-        return out
+        if not Parser._is_package_line(line):
+            return None
+        packages: list[DebPakInfo] = list()
+
+        # remove <list-bullet>
+        _line: str = line.strip()[1:].strip()
+        # remove :: <comment>
+        _line = _line.split("::")[0].strip()
+        # parse packages and info
+        packages.extend([Parser._get_package_info(pak) for pak in _line.split(",")])
+
+        # filter packages (by adding a score)
+        # if a package lacks distro or release information (=None) treat it
+        # to match any distro or release. If an exact match for distro or
+        # distro and release can be found return this.
+        kept_packages: list[DebPakInfo] = Parser._matching_packages(packages, distro, release)
+
+        if len(kept_packages) < 1:
+            return None
+
+        # narrow by release
+        if len(kept_packages) > 1:
+            if any([p.release is not None for p in kept_packages]):
+                kept_packages = [p for p in kept_packages if p.release is not None]
+        # narrow by distro
+        if len(kept_packages) > 1:
+            if any([p.distro is not None for p in kept_packages]):
+                kept_packages = [p for p in kept_packages if p.distro is not None]
+
+        # still more than 1 package -> error
+        if len(kept_packages) > 1:
+            msg = "More than two packages match the distro and release requirements"
+            raise ValueError(f"{msg}: {', '.join([p.name for p in kept_packages])}.")
+        return kept_packages[0]
+
+    @staticmethod
+    def _matching_packages(packages: Sequence[DebPakInfo], distro: str, release: str) -> list[DebPakInfo]:
+        """Filter packages by distro and release."""
+        matching: list[DebPakInfo] = list()
+        for package in packages:
+            if package.release is not None and package.release != release:
+                continue
+            if package.distro is not None and package.distro != distro:
+                continue
+            if package not in matching:
+                matching.append(package)
+        return matching
 
     @staticmethod
     def _is_package_line(line: str) -> bool:
@@ -57,10 +101,21 @@ class Parser:
         return True if check else False
 
     @staticmethod
-    def _get_package_info(line: str) -> DebPakInfo:
-        pak = Parser.PACKAGE_AND_INFO_RX.match(line)
+    def _get_package_info(string: str) -> DebPakInfo:
+        """
+        Extract debian package information from a line of the form:
+        <package-name> {<distro-name>:<release>}
+        """
+        package_info_regex: re.Pattern = re.compile(
+            "\\s*(?P<package_name>[-\\w]+)" +
+            "(\\s+[{]\\s*" +
+            "(?P<distro_name>[\\w]+)" +
+            "(\\s*:\\s*(?P<release>[\\w.]+))?" +
+            "\\s*[}])?"
+        )
+        pak = package_info_regex.match(string)
         if not pak:
-            raise ValueError("Not a valid package line.")
+            raise ValueError("Not a valid package string.")
         return DebPakInfo(
             name=pak.group("package_name"),
             distro=pak.group("distro_name"),
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -60,24 +60,59 @@ def package_info_str_input() -> Sequence[tuple[str, DebPakInfo]]:
 
 
 @pytest.fixture
-def deb_package_line_input() -> Sequence[tuple[str, Sequence[DebPakInfo]]]:
+def deb_package_line_input() -> Sequence[tuple[str, str, str, DebPakInfo]]:
     return [
         ("This is not a package line...",
-         []),
+         "any_distro", "any_release",
+         None),
+
         ("This neither but it has a + and a - in it.",
-         []),
+         "any_distro", "any_release",
+         None),
+
         (" + package1",
-         [DebPakInfo(name="package1")]),
-        (" - package-x {Debian} ",
-         [DebPakInfo(name="package-x", distro="Debian")]),
-        ("+ pak-a {Debian}, pak-b {theOtherDistro:42} ",
-         [DebPakInfo(name="pak-a", distro="Debian"),
-          DebPakInfo(name="pak-b", distro="theOtherDistro", release="42")]),
+         "any_distro", "any_release",
+         DebPakInfo(name="package1")),
+
+        (" - package-x {Distro} ",
+         "not_distro", "any_release",
+         None),
+
+        (" - package-x {Distro} ",
+         "Distro", "any_release",
+         DebPakInfo(name="package-x", distro="Distro")),
+
+        ("+ pak-a {Debian}, pak-b {theOtherDistro:42}, pak-c ",
+         "any_distro", "any_release",
+         DebPakInfo(name="pak-c")),
+
+        ("+ pak-a {Debian}, pak-b {theOtherDistro:42}, pak-c ",
+         "Debian", "any_release",
+         DebPakInfo(name="pak-a", distro="Debian")),
+
+        ("+ pak-a {Debian}, pak-b {theOtherDistro:42}, pak-c ",
+         "theOtherDistro", "any_release",
+         DebPakInfo(name="pak-c")),
+
+        ("+ pak-a {Debian}, pak-b {theOtherDistro:42}, pak-c ",
+         "theOtherDistro", "42",
+         DebPakInfo(name="pak-b", distro="theOtherDistro", release="42")),
+
         ("  - pakx :: this is a great package!",
-         [DebPakInfo(name="pakx")]),
+         "any_distro", "any_release",
+         DebPakInfo(name="pakx")),
+
         ("+ pak-all, pak-special {forSpecial:321} :: special needs special + love",
-         [DebPakInfo(name="pak-all"),
-          DebPakInfo(name="pak-special", distro="forSpecial", release="321")])
+         "any_distro", "any_release",
+         DebPakInfo(name="pak-all")),
+
+        ("+ pak-all, pak-special {forSpecial:321} :: special needs special + love",
+         "forSpecial", "other_release",
+         DebPakInfo(name="pak-all")),
+
+        ("+ pak-all, pak-special {forSpecial:321} :: special needs special + love",
+         "forSpecial", "321",
+         DebPakInfo(name="pak-special", distro="forSpecial", release="321"))
     ]
 
 
@@ -85,16 +120,19 @@ class TestParser:
     """Tests for the parser itself."""
 
     def test_get_package_info(self, package_info_str_input):
-        for line, pakinfo in package_info_str_input:
-            assert Parser._get_package_info(line) == pakinfo
+        for line, pak_info in package_info_str_input:
+            assert Parser._get_package_info(line) == pak_info
 
     def test_line_containing_no_package_info_returns_empty_list(self, non_package_lines):
         for line in non_package_lines:
-            output: list[DebPakInfo] = Parser._extract_deb_packages_from_line(line)
-            assert output == []
+            output: DebPakInfo | None = Parser.extract_deb_package_from_line(line, "any", "any")
+            assert output is None
 
-    def test_extract_deb_packages(self, deb_package_line_input):
-        for line, expected_list in deb_package_line_input:
-            package_list = Parser._extract_deb_packages_from_line(line)
-            assert len(package_list) == len(expected_list)
-            assert package_list == expected_list
+    def test_extract_deb_package_from_line(self, deb_package_line_input):
+        for line, *param, expected_DebPakInfo in deb_package_line_input:
+            package_info = Parser.extract_deb_package_from_line(line, *param)
+            assert package_info == expected_DebPakInfo
+
+    def test_extract_deb_packages_file(self):
+        # TODO !!
+        pass
