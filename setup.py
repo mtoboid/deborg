870741608a970801dc373cc122b1fe31941d52c5
Tobias Marczewski (mtoboid) <vortex@e.mail.de>
2022-02-03 15:14:04 +0100

    Added files required for packaging.     * added pyproject.toml     * moved information from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,11 +1,1 @@
-from setuptools import setup, find_packages
 
-setup(
-    name="deborg",
-    version="0.0.1",
-    description="Extract .deb packages from an emacs orgmode file.",
-    author="Tobias Marczewski",
-    extras_require=dict(tests=['pytest']),
-    packages=find_packages(where='src'),
-    package_dir={"": "src"}
-)
