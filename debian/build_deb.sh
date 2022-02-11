1119919df4786317d273b76306b2707fcbf9dd46
Tobias Marczewski (mtoboid) <vortex@e.mail.de>
2022-02-11 15:54:40 +0100

    Work on build script.

---
+++ debian/build_deb.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+# script to build a debian package for deborg
+
+# 1) ensure we have a venv
+# TODO
+
+# 2) activate the python venv and build the python package
+# TODO
+
+# 3) build the debian package
+
+main() {
+    echo "main"
+}
+
+main "$@"
