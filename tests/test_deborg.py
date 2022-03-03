2d293e471cf84a0131fbecdf1d54c90786eb3632
Tobias Marczewski (mtoboid) <vortex@e.mail.de>
2022-03-03 18:46:19 +0100

    Added tests for the examples.

--- tests/test_deborg.py
+++ tests/test_deborg.py
@@ -4,8 +4,9 @@ Tests for the command line utility deborg accessed via deborg.__main__.py.
 
 from __future__ import annotations
 
-import pytest
-from pytest_console_scripts import script_runner
+from pytest_console_scripts import RunResult
+
+from deborg.cli import Examples
 
 
 def test_two_package_match_in_same_line_raises_error(tmpdir, script_runner):
@@ -64,7 +65,15 @@ def test_parse_testfile(script_runner):
 
 
 def test_example_file_examples(tmpdir, script_runner):
-    # orgfile = tmpdir.join("testfile.org")
-    # orgfile.write(file_content)
-    # result = script_runner.run('deborg', str(orgfile), 'distro', 'release')
-    assert 0
+    tests: Examples = Examples()
+    orgfile = tmpdir.join("testfile.org")
+    command_out = script_runner.run('deborg', '--example-file')
+    orgfile.write(command_out.stdout)
+    for i in range(tests.n):
+        # the parsed input 'distro1' becomes "'distro1'" which is not "distro1"
+        # -> remove all ' from the strings
+        args = [a.replace("'", "") for a in tests.input[i].split(' ')]
+        command = ['deborg', str(orgfile), *args]
+        result: RunResult = script_runner.run(*command)
+        assert result.returncode == 0
+        assert result.stdout.strip() == tests.output[i].strip()
